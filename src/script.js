942a17cbcb8874e4b02fe5494e1d53e27f1166dc
salekmichal1 <michal.salek@microsoft.wsei.edu.pl>
2025-02-08 23:30:42 +0100

    update countdown date to May 19, 2025

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ window.onload = function () {
   document.body.style.display = 'block';
 };
 
-const countToDate = new Date('Jan 30, 2025 17:03:00').getTime();
+const countToDate = new Date('May 19, 2025 17:03:00').getTime();
 let previousTimeBetweenDates;
 setInterval(() => {
   const currentDate = new Date().getTime();
